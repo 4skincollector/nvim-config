c149396868d1a507a8a24a5c8419e8851b393e73
4skincollector <piotrmeson2@gmail.com>
2025-07-09 19:55:01 +0200

    Added plugins, basic files, configuration, keymaps
    Added the catppuccin theme plugin and set it as the default theme in config.colorscheme (mocha flavor). Added basic keymaps in config.keymappings. Added the nvim-tree plugin with defaults. Added a config.opts file for future configuration.

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>o', 'o<ESC>')
+vim.keymap.set('n', '<leader>O', 'O<ESC>')
+vim.keymap.set('n', '<leader>', '<Nop>')
+vim.keymap.set('i', '<C-h>', '<ESC>i')
+vim.keymap.set('i', '<C-j>', '<ESC>lji')
+vim.keymap.set('i', '<C-k>', '<ESC>lki')
+vim.keymap.set('i', '<C-l>', '<ESC>lli')
+
